6f2dc4a9fb6e602b0120157a0e5796b26de04dbd
kihongChoi <kihong08@gmail.com>
2016-10-23 17:55:10 +0900

    goto.m moved

---
+++ goto.m
@@ -0,0 +1,13 @@
+classdef goto
+    methods (Static)
+        function git()
+            cd C:\Users\finch\Documents\MATLAB\git\doa_holo;
+        end
+        
+        function finch()
+            cd D:\FINCH_playground;
+        end
+    end
+    
+end
+
